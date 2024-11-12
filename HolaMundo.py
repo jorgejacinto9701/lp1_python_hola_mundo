b2c60a888b9e544bbb4f54ef376af46d633da42e
JavierDev39 <enmanueldelaza@gmail.com>
2024-11-11 20:38:48 -0500

    subir cambios

--- HolaMundo.py
+++ HolaMundo.py
@@ -12,3 +12,5 @@ print ("Hola Mundo => new Jorge Jacinto ==> G1")
 
 print ("Hola Mundo => new Mauricio Lozada ==> G2")
 print ("Hola Mundo => Vasque Rojas ==> G2")
+
+print ("Hola Munde => De la Cruz Javiercito ==> G3")
